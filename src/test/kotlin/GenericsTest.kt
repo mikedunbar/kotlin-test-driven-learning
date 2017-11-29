6475eef8edcb764725c795213fab955120c4ce3f
mikedunbar <miketdunbar@gmail.com>
2017-11-29 12:05:52 -0500

    more generic examples

--- src/test/kotlin/GenericsTest.kt
+++ src/test/kotlin/GenericsTest.kt
@@ -1,3 +1,4 @@
+import foo.yours
 import org.amshove.kluent.shouldBeInstanceOf
 import org.amshove.kluent.shouldEqual
 import org.jetbrains.spek.api.Spek
@@ -6,6 +7,7 @@ import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
+import org.omg.CORBA.INTERNAL
 
 @RunWith(JUnitPlatform::class)
 class GenericsTest : Spek({
@@ -175,17 +177,120 @@ class GenericsTest : Spek({
             } catch (e: Exception) {
                 caught = e
             }
-            caught.javaClass shouldEqual IllegalArgumentException().javaClass
+            caught.javaClass shouldEqual IllegalArgumentException::class.java
         }
     }
 
-    describe("Declaring function with reified types") {
+    describe("Declaring functions with reified type params") {
+        it("allows you to refer to the type arguments at runtime") {
+            // See inLineIsA below
+            inlineIsA<Int>("hi") shouldEqual false
+            inlineIsA<String>("hi") shouldEqual true
+        }
 
+        it("is used by the standard library function - ") {
+            val items = listOf("one", 2, "three")
+            items.filterIsInstance<String>() shouldEqual listOf("one", "three")
+        }
 
+        it("is not possible to call an inline function with reified type params from Java") {
+
+        }
     }
 
+    describe("variance") {
+        it("describes how a types with the same base type and different type args relate") {
+            //e.g. List<String> and List<Any>
+        }
 
+        it("is possible to pass List<String> for List<Any> immutable function arg") {
+            fun asString(list: List<Any>) : String {
+               return list.joinToString()
+            }
 
+            asString(listOf(1,2,3)) shouldEqual listOf(1,2,3).joinToString()
+        }
+
+        it("is NOT possible to pass MutableList<String> for MutableList<Any> mutable function args") {
+            fun addNum(list: MutableList<Any>) {
+                list.add(5)
+            }
+
+            var strings : MutableList<String> = mutableListOf("hi", "there");
+            // uncomment for error
+            //addNum(strings)
+        }
+    }
+
+    describe("concepts of class, type, and subtype") {
+        it("is the case that every kotlin class can construct 2 types - nullable and non-nullable") {
+            val x : String? = null
+            val y: String = "abc"
+        }
+
+        it("is the case that each generic class produces an infinite number of types") {
+            val x : List<String>
+            val y : List<SealedOuter>
+            //...
+        }
+
+        it("happens that type B is a subtype of a type A if you can use the value of the type B whenever a value of the type A is required.") {
+            val x: Int = 7
+            val y: String = "7"
+            fun isNumber(z: Any) = z is Number
+
+            isNumber(x) shouldEqual true
+            isNumber(y) shouldEqual false
+        }
+
+        it("happens that every type is a subtype of itself") {
+            val x: Number = 7
+            val y: Int = 6
+            fun isNumber(z: Any) = z is Number
+            fun isInt(w: Any) = w is Int
+
+            isNumber(x) shouldEqual true
+            isInt(y) shouldEqual true
+        }
+
+        it("is not possible to assign a value to a variable when the value type isn't a subtype of the variable type") {
+            var x : Number
+            val y: Int = 7
+            val z: String = "7"
+
+            x = y // fine
+
+            //uncomment for error
+            //x = z
+        }
+
+        it("happens that a non-null type is a subtype of it's nullable counterpart") {
+            var x: String?
+            val y: String = "y"
+
+            x = y //fine
+        }
+
+        it("happens that a nullable type is NOT a subtype of it's non-nullable counterpart") {
+            var x: String
+            val y: String? = "y"
+
+            //uncomment for error
+            //x = y
+        }
+    }
+
+    describe("When a generic class or interface is called 'invariant on the type parameter'") {
+        it("is when for any two types A & B, GenericClass<A> isn't a subtype or supertype of GenericClass<B>") {
+
+        }
+    }
+
+    describe("when a generic class or interface is called 'covariant'") {
+        it("is when for any two types A & B, GenericClass<A> is a subtype of GenericClass<B> when A is a subtype of B") {
+            
+        }
+    }
 })
 
 interface Seer {
@@ -215,3 +320,5 @@ class GenericClass<T> (val x: T) {
 interface MyList<T> {
     fun elementAt(index: Int) : T
 }
+
+inline fun <reified T> inlineIsA(a: Any) = a is T
